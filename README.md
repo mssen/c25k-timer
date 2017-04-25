22911dfcdc52d4d731b1188930d6100af31dba20
mssen <missy.danielsen@gmail.com>
2017-04-25 15:43:39 -0500

    Initial push with simple timer and data

---
+++ README.md
@@ -0,0 +1,13 @@
+# C25K Timer
+
+A simple app to keep track of intervals for the Couch to 5K program. Timings gathered from [C25K.com](http://www.c25k.com/c25k_treadmill.html).
+
+## Features
+
+- Visual guid for remaining time and current interval
+- Ding to signify interval change
+- Uses local storage to track progress
+
+## TODO
+
+- [ ] Robo voice for run/walk
---
+++ src/components/Timer.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+
+class Timer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { seconds: 0 }
+  }
+
+  tick() {
+    this.setState((prevState) => ({
+      seconds: prevState.seconds + 1
+    }));
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => this.tick(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  format(seconds) {
+    const remainderSeconds = seconds % 60;
+    return Math.floor(seconds / 60) + ':' + (remainderSeconds < 10 ? '0' : '') + remainderSeconds;
+  }
+
+  render() {
+    return (
+      <div>
+        Total Time: { this.format(this.state.seconds) }
+      </div>
+    );
+  }
+}
+
+export default Timer;
