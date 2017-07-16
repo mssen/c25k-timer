622d5f80252bfe1839819f97ee8e3da9748cadea
mssen <missy.danielsen@gmail.com>
2017-07-16 13:33:28 -0500

    Added test for IntervalTracker.

--- src/components/IntervalTracker.test.jsx
+++ src/components/IntervalTracker.test.jsx
@@ -5,3 +5,19 @@ import IntervalTracker from './IntervalTracker';
 it('renders without crashing', () => {
   shallow(<IntervalTracker />);
 });
+
+it('shows Warmup when current interval is 0', () => {
+  const wrapper = shallow(<IntervalTracker current={ 0 } total={ 4 } />);
+  expect(wrapper).toMatchSnapshot();
+})
+
+it('shows Cooldown when current interval is more than total', () => {
+  const wrapper = shallow(<IntervalTracker current={ 5 } total={ 4 } />);
+  expect(wrapper).toMatchSnapshot();
+})
+
+it('shows current interval', () => {
+  const wrapper = shallow(<IntervalTracker current={ 1 } total={ 4 } />);
+  expect(wrapper).toMatchSnapshot();
+})
+
