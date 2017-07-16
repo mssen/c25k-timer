1c6f856efefa12954c1e515c511307c4188e0171
mssen <missy.danielsen@gmail.com>
2017-07-16 13:17:52 -0500

    Added Enzyme and basic tests for all components.

--- src/components/PickerModal.test.jsx
+++ src/components/PickerModal.test.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import App from './App';
+import PickerModal from './PickerModal';
 
 it('renders without crashing', () => {
-  shallow(<App />);
+  shallow(<PickerModal segments={ [] } />);
 });
