fb4658160dd75b2c541bea2ad5d7df494ad8329a
mssen <missy.danielsen@gmail.com>
2017-07-19 21:18:12 -0500

    Added tests for Navigation.

--- src/components/Navigation.test.jsx
+++ src/components/Navigation.test.jsx
@@ -5,3 +5,41 @@ import Navigation from './Navigation';
 it('renders without crashing', () => {
   shallow(<Navigation />);
 });
+
+describe('renders the correct previous and next week/day', () => {
+  it('on last day of week', () => {
+    const wrapper = shallow(<Navigation week={ 1 } day={ 3 } />);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('on first day of week', () => {
+    const wrapper = shallow(<Navigation week={ 3 } day={ 1 } />);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('in middle of week', () => {
+    const wrapper = shallow(<Navigation week={ 1 } day={ 2 } />);
+    expect(wrapper).toMatchSnapshot();
+  });
+});
+
+it('handles navigation to the previous day', () => {
+  const triggerNavigationStub = jest.fn();
+  const wrapper = shallow(<Navigation triggerNavigation={ triggerNavigationStub } />);
+  wrapper.find('button').at(0).simulate('click');
+  expect(triggerNavigationStub).toBeCalledWith(-1);
+});
+
+it('handles navigation to the next day', () => {
+  const triggerNavigationStub = jest.fn();
+  const wrapper = shallow(<Navigation triggerNavigation={ triggerNavigationStub } />);
+  wrapper.find('button').at(2).simulate('click');
+  expect(triggerNavigationStub).toBeCalledWith(1);
+});
+
+it('handles show modal', () => {
+  const showModalStub = jest.fn();
+  const wrapper = shallow(<Navigation showModal={ showModalStub } />);
+  wrapper.find('button').at(1).simulate('click');
+  expect(showModalStub).toBeCalled();
+});
