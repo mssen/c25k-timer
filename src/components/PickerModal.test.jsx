02840c3ba9cc1be88acdd04c6f2a988ee00060e3
mssen <missy.danielsen@gmail.com>
2017-07-19 21:34:23 -0500

    Added tests for PickerModal.

--- src/components/PickerModal.test.jsx
+++ src/components/PickerModal.test.jsx
@@ -2,6 +2,34 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import PickerModal from './PickerModal';
 
+const segments = [
+    { week: 1, day: 1 },
+    { week: 1, day: 2 },
+    { week: 1, day: 3 },
+    { week: 2, day: 1 },
+    { week: 2, day: 2 },
+    { week: 2, day: 3 }
+  ];
+
 it('renders without crashing', () => {
   shallow(<PickerModal segments={ [] } />);
+});
+
+it('renders weeks and days', () => {
+  const wrapper = shallow(<PickerModal segments={ segments } />);
+  expect(wrapper).toMatchSnapshot();
+});
+
+it('handles hiding the modal', () => {
+  const hideModalStub = jest.fn();
+  const wrapper = shallow(<PickerModal segments={ [] }  hideModal={ hideModalStub } />);
+  wrapper.find('button').first().simulate('click');
+  expect(hideModalStub).toBeCalled();
+});
+
+it('handles updating the segment', () => {
+  const updateSegmentStub = jest.fn();
+  const wrapper = shallow(<PickerModal segments={ segments } currentSegment={ 3 }  updateSegment={ updateSegmentStub } />);
+  wrapper.find('div > button').first().simulate('click');
+  expect(updateSegmentStub).toBeCalledWith(-3);
 });
