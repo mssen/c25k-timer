d7a7b42959f93f1c7b85a84d6c079ab4b3f71854
mssen <missy.danielsen@gmail.com>
2017-07-25 22:31:26 -0500

    Planned out some tests for Timer

--- src/components/Timer.test.jsx
+++ src/components/Timer.test.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import Timer from './Timer';
 
 it('renders without crashing', () => {
   shallow(<Timer intervals={ [10] } />);
+});
+
+it('is paused when created', () => {
+  const wrapper = mount(<Timer intervals={ [10] } />);
+});
+
+it('can start');
+
+it('can pause');
+
+it('switches over to the next segment');
+
+describe('next interval', () => {
+  it('goes on to the next interval');
+
+  it('stops when it was the last interval');
 });
