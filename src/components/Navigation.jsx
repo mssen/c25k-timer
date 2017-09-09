84da4754093efa576f5ead17503c9ec6770048dc
mssen <missy.danielsen@gmail.com>
2017-09-08 21:39:25 -0500

    Navigation button styling.

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import './Navigation.css';
+
 const Navigation = ({ week, day, triggerNavigation, showModal }) => {
   const handleNext = () => {
     triggerNavigation(1);
@@ -16,12 +18,16 @@ const Navigation = ({ week, day, triggerNavigation, showModal }) => {
     <div>
       {
         !(week === 1 && day === 1)
-          && <button onClick={ handlePrevious }>&lsaquo;&lsaquo; Week { day === 1 ? week - 1 : week }, Day { day === 1 ? 3 : day - 1 }</button>
+          && <button className="nav-button" onClick={ handlePrevious }>
+              &lsaquo;&lsaquo; Week { day === 1 ? week - 1 : week }, Day { day === 1 ? 3 : day - 1 }
+            </button>
       }
-      <button onClick={ showModal }>...</button>
+      <button className="nav-button" onClick={ showModal }>...</button>
       {
         !(week === 9 && day === 3)
-          && <button onClick={ handleNext }>Week { day === 3 ? week + 1 : week }, Day { day === 3 ? 1 : day + 1 } &rsaquo;&rsaquo;</button>
+          && <button className="nav-button" onClick={ handleNext }>
+              Week { day === 3 ? week + 1 : week }, Day { day === 3 ? 1 : day + 1 } &rsaquo;&rsaquo;
+            </button>
       }
     </div>
   )
